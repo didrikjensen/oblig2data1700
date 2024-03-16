4e211417fa30d893be83104c751fe704190dd550
didrikjensen <142822246+didrikjensen@users.noreply.github.com>
2024-03-16 13:39:34 +0100

    ye

---
+++ src/test/java/com/example/oblig2v2/Oblig2v2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig2v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig2v2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
